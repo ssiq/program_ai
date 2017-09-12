cd33376661ac0866ba81d21163bc00d6832afdab
Liwei Wu <liweiwuhome@hotmail.com>
2017-09-12 21:20:01 +0800

    add cache dataset function

--- util.py
+++ util.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
-import os
 import typing
+import functools
+import pickle
+import errno
+import os
 
 
 def make_dir(*path: str) -> None:
@@ -36,6 +39,48 @@ def format_dict_to_string(to_format_dict: dict) -> str:
     """
 
     return '__'.join(str(a)+str(b) for a, b in to_format_dict.items())
+
+
+def ensure_directory(directory):
+    """
+    Create the directories along the provided directory path that do not exist.
+    """
+    directory = os.path.expanduser(directory)
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise e
+
+def disk_cache(basename, directory, method=False):
+    """
+    Function decorator for caching pickleable return values on disk. Uses a
+    hash computed from the function arguments for invalidation. If 'method',
+    skip the first argument, usually being self or cls. The cache filepath is
+    'directory/basename-hash.pickle'.
+    """
+    directory = os.path.expanduser(directory)
+    ensure_directory(directory)
+
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            key = (tuple(args), tuple(kwargs.items()))
+            # Don't use self or cls for the invalidation hash.
+            if method and key:
+                key = key[1:]
+            filename = '{}-{}.pickle'.format(basename, hash(key))
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                with open(filepath, 'rb') as handle:
+                    return pickle.load(handle)
+            result = func(*args, **kwargs)
+            with open(filepath, 'wb') as handle:
+                pickle.dump(result, handle)
+            return result
+        return wrapped
+
+    return wrapper
 
 
 if __name__ == '__main__':
