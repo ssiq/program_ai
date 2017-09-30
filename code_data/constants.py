498823334a91bca7b8aabb8734a95e6a1a32c6c8
lfsoftware13 <lfsoftware13@gmail.com>
2017-10-01 04:51:13 +0800

    modify database path

--- code_data/constants.py
+++ code_data/constants.py
@@ -11,7 +11,7 @@ FAKE_ERROR_CODE = 'fake_error_code'
 
 spider_key_path = 'key/id_rsa_spider'
 remote_db_path = '/home/lf/DataCrawl/scrapy_OJ/release_data/scrapyOJ.db'
-local_db_path = '/home/lf/Project/program_ai/data/scrapyOJ1.db'
+local_db_path = '/home/lf/Project/program_ai/data/scrapyOJ.db'
 
 spider_ip = '218.94.159.108'
 spider_port = 22
